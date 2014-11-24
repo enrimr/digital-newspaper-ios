3152b34947a3af8d9fb7c797af1a8393bbc47222
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2014-11-24 13:36:12 +0100

    Initial commit

---
+++ elmundo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  elmundo
+//
+//  Created by Enrique Ismael Mendoza Robaina on 24/11/14.
+//  Copyright (c) 2014 cuaQea SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ elmundo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  elmundo
+//
+//  Created by Enrique Ismael Mendoza Robaina on 24/11/14.
+//  Copyright (c) 2014 cuaQea SL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
